b871204a21261959ba3670534ed34b0277812d2f
Megha2522 <111370791+Megha2522@users.noreply.github.com>
2022-08-23 15:34:30 +0100

    fouth question

---
+++ Competitive Programming/CodeForces/VK Cup 2012 Qualification Round 1/A. Next Round.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int a[n]; int c=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>=a[k-1] && a[i]!=0)
+        ++c;
+        
+    }
+    cout<<c;
+    return 0;
+}
